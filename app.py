7ef143f4d42c9f3ed493edd16d040a38ab281554
vivek <viveksood92@gmail.com>
2023-11-07 23:25:18 +0530

    Add company website

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_mail import Mail, Message
+from flask import request,flash
+app = Flask(__name__)
+
+app.config.from_object('config.Config')
+
+mail = Mail(app)
+
+@app.route('/' , methods =["GET", "POST"] )
+def hello():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       print(request.form.get("email"))
+       flash(f'Thanks for submiit created', 'success')
+    return render_template('index.html')
+
+@app.route('/form' , methods =["POST"] )
+def form_data():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       name = request.form.get("name")
+       email = request.form.get("email")
+       phone_no = message = request.form.get("phone")
+       message = request.form.get("message")
+       subject = "New Client Query"
+       msg = Message(subject, sender = 'support@qubitnets.com', recipients = ['support@qubitnets.com'])
+       msg.body = f"Hey qubitnets, {name} is trying to contact us. Email : {email} , Phone_no {phone_no},message {message}, "
+       mail.send(msg)
+       return "Success",200
+
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=80)
+
+# @app.route("/send")
+# def index():
+#     msg = Message('Hello from the other side!', sender = 'support@qubitnets.com', recipients = ['viveksood92@gmail.com'])
+#     msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
+#     mail.send(msg)
+#     return "Message sent!"
---
+++ config.py
@@ -0,0 +1,25 @@
+"""Flask APP configuration."""
+from os import environ, path
+from dotenv import load_dotenv
+
+
+# Specificy a `.env` file containing key/value config values
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+
+# General Config
+class Config:
+    """Set Flask config variables."""
+
+    # General Config
+    ENVIRONMENT = environ.get("ENVIRONMENT")
+    SECRET_KEY = environ.get("SECRET_KEY")
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = environ.get("MAIL_PASSWORD")
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
